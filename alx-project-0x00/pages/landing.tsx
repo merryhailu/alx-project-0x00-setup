486aaccb1c5f4cc552adb9b60cb23044d254eba4
Meron Hailu <hailumeron03@gmail.com>
2025-06-01 13:15:59 +0300

    feat: adding week task

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -11,21 +11,21 @@ const Landing: React.FC =  () => {
         <div className="flex gap-4">
           <Button title="Small - sm" styles="text-sm rounded-sm" />
           <Button title="Small - md" styles="text-sm rounded-md" />
-          <Button title="Small - lg" styles="text-sm rounded-lg" />
+          <Button title="Small - lg" styles="text-sm rounded-full" />
         </div>
 
         <h2 className="font-semibold">Medium Buttons</h2>
         <div className="flex gap-4">
           <Button title="Medium - sm" styles="text-base rounded-sm" />
           <Button title="Medium - md" styles="text-base rounded-md" />
-          <Button title="Medium - lg" styles="text-base rounded-lg" />
+          <Button title="Medium - lg" styles="text-base rounded-full" />
         </div>
 
         <h2 className="font-semibold">Large Buttons</h2>
         <div className="flex gap-4">
           <Button title="Large - sm" styles="text-lg px-6 py-3 rounded-sm" />
           <Button title="Large - md" styles="text-lg px-6 py-3 rounded-md" />
-          <Button title="Large - lg" styles="text-lg px-6 py-3 rounded-lg" />
+          <Button title="Large - lg" styles="text-lg px-6 py-3 rounded-full" />
         </div>
       </div>
     )
